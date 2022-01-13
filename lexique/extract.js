7a0a5949571471530469bc8b33193cf435a0bb68
Simon Cambier <simon.cambier@protonmail.com>
2022-01-13 18:51:07 +0100

    List of simpler words
    + keep accents for the wiktionnaire link

--- lexique/extract.js
+++ lexique/extract.js
@@ -1,19 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.readFile(path.join(__dirname, '/lexique.txt'), function (err, data) {
+fs.readFile(path.join(__dirname, '/liste-reduite.txt'), function (err, data) {
   if (err) {
     throw err
   }
   const words = data.toString().split(/\r?\n/).join(' ').split(' ')
-  const filtered = words
-    .filter(o => o.length === 5)
-    .filter(o => /^[\u0000-\u007f]*$/.test(o))
+  const filtered = words.filter(o => o.length === 5)
+  // Convert accents
+  // .map(o => o.normalize('NFD').replace(/[\u0300-\u036f]/g, ''))
+  // Remove words with accents
+  // .filter(o => /^[\u0000-\u007f]*$/.test(o))
   console.log(filtered.length)
   const set = new Set(filtered)
 
   fs.writeFileSync(
-    path.join(__dirname, '/../src/words.ts'),
+    path.join(__dirname, '/../src/words-list.ts'),
     `const words = ${JSON.stringify(
       [...set].map(o => o.toLowerCase()),
     )}; export default words`,
