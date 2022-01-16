f944983b4c4960a6ba9e7cda61d6fdce87c4d4a6
Simon Cambier <simon.cambier@protonmail.com>
2022-01-16 10:34:25 +0100

    Removed proper nouns

--- lexique/extract.js
+++ lexique/extract.js
@@ -6,7 +6,13 @@ fs.readFile(path.join(__dirname, '/liste-reduite.txt'), function (err, data) {
   if (err) {
     throw err
   }
-  const words = data.toString().split(/\r?\n/).join(' ').split(' ')
+  const words = data
+    .toString()
+    .split(/\r?\n/)
+    // Remove proper nouns
+    .filter(o => o[0] === o[0].toLowerCase())
+    .join(' ')
+    .split(' ')
   const filtered = words.filter(o => o.length === 5)
   const set = [...new Set(filtered)]
   console.log(set.length + ' words')
