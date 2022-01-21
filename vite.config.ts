24b328e0e5c5cc53f0c7b90d33c97324dd32a0ff
Simon Cambier <simon.cambier@protonmail.com>
2022-01-21 18:26:52 +0100

    PWA base setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 process.env.VITE_BUILD_DATE = new Date().toISOString()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/momomotus/',
-  plugins: [vue(), Icons({ autoInstall: true })],
+  plugins: [vue(), Icons({ autoInstall: true }), VitePWA({})],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
