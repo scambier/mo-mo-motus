c548d5b4f4fe9945c2209afe01f92d2647cd8669
Simon Cambier <simon.cambier@protonmail.com>
2022-01-24 20:30:22 +0100

    PWA setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import './index.css'
 
+import { registerSW } from 'virtual:pwa-register'
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import { showToast } from './composables/toast-manager'
 import router from './router'
+
+registerSW({
+  onOfflineReady() {
+    showToast(
+      'Cette application peut maintenant<br>fonctionner hors-ligne.',
+      2000,
+    )
+  },
+})
 
 const app = createApp(App)
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,43 @@ process.env.VITE_BUILD_DATE = new Date().toISOString()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/momomotus/',
-  plugins: [vue(), Icons({ autoInstall: true }), VitePWA({})],
+  base: '/momomotus/pwa',
+  plugins: [
+    vue(),
+    Icons({ autoInstall: true }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.png',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'Mo-Mo-Motus',
+        short_name: 'Motus',
+        description: 'Découvrez le mot secret en six essais',
+        theme_color: '#198A42',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
