91ff17365bc4c338bc6c56a1bde6241d211213ba
Simon Cambier <simon.cambier@protonmail.com>
2022-01-28 23:16:21 +0100

    Updated words list

--- lexique/extract.js
+++ lexique/extract.js
@@ -13,8 +13,22 @@ fs.readFile(path.join(__dirname, '/liste-reduite.txt'), function (err, data) {
     .filter(o => o[0] === o[0].toLowerCase())
     .join(' ')
     .split(' ')
-  const filtered = words.filter(o => o.length === 5)
-  const set = [...new Set(filtered)]
+
+  // First filter
+  const filtered = words.filter(
+    w =>
+      w.length === 5 &&
+      !['cedex', 'nègre', 'conne', 'sicav', 'vulgo'].includes(w),
+  )
+
+  // Removed identical words, with different accents
+  const withoutDupes = []
+  for (const word of filtered) {
+    if (!withoutDupes.map(normalize).includes(normalize(word))) {
+      withoutDupes.push(word)
+    }
+  }
+  const set = [...new Set(withoutDupes)]
   console.log(set.length + ' words')
 
   fs.writeFileSync(
@@ -39,12 +53,13 @@ fs.readFile(path.join(__dirname, '/ods6.txt'), function (err, data) {
   fs.writeFileSync(
     path.join(__dirname, '/../src/guesses-list.ts'),
     `const guesses = ${JSON.stringify(
-      set.map(o =>
-        o
-          .toLowerCase()
-          .normalize('NFD')
-          .replace(/[\u0300-\u036f]/g, ''),
-      ),
+      set.map(normalize),
     )}; export default guesses`,
   )
 })
+
+function normalize(word) {
+  return word.toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+}
