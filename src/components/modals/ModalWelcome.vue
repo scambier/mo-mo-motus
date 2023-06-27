225cdb9991b3e4dd801fef13ab96c1b4fc0d0d70
Simon Cambier <simon.cambier@protonmail.com>
2023-06-27 12:44:44 +0200

    removed old "nouveauté"

--- src/components/modals/ModalWelcome.vue
+++ src/components/modals/ModalWelcome.vue
@@ -13,14 +13,6 @@
       Une lettre mal placée est 🟨.<br>
       Une lettre qui n'est pas dans le mot reste ⬛.
     </p>
-    <div class="mb-4 border-y">
-      <strong>✨ Nouveauté ✨</strong><br>
-      <div class="text-sm">
-        Du mal à trouver un mot ? Utilisez
-        <span class="px-1 rounded border">?</span>
-        pour remplir les cases manquantes et vous aider à visualiser !
-      </div>
-    </div>
     <div>
       <ButtonGreen @click="isVisibleModalWelcome = false">
         C'est parti !
